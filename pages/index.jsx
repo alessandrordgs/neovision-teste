cdf7a46128215cda34b26687089ba9608032600a
forge1010 <ale.silva1120@gmail.com>
2021-09-19 01:14:57 -0300

    feat: title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
@@ -5,6 +7,10 @@ import Main from "./Components/Main";
 export default function Home() {
   return (
     <>
+    <Head>
+       <title>NeoVision</title>
+    </Head>
+    
     <Header/> 
     <Main/>
     <Footer/>
